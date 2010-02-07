cafb29ec557015266f669edb8f231502197ee306
Scott Jackson <scottjacksonx@gmail.com>
2010-02-08 09:13:16 +1000

    Updated hnapi.py

--- hnapi.py
+++ hnapi.py
@@ -1,3 +1,39 @@
+"""
+hn-api is a simple, ad-hoc Python API for Hacker News.
+===========================================================
+
+hn-api is released under the Simplified BSD License:
+
+Copyright (c) 2010, Scott Jackson
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY SCOTT JACKSON ``AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SCOTT JACKSON OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of Scott Jackson.
+
+
+"""
+
 from BeautifulSoup import BeautifulSoup
 import urllib2
 
@@ -58,7 +94,7 @@ class HackerNewsAPI:
 		domain = source[domainStart:domainEnd]
 		# Check for "Ask HN" links.
 		if domain[0] == '=':
-			domain = "http://news.ycombinator.com"
+			return "http://news.ycombinator.com"
 		return "http://" + domain[1:len(domain)-2]
 		
 	def getStoryTitle(self, source):
@@ -261,3 +297,11 @@ class HackerNewsUser:
 		karmaStart = source.find('<td valign=top>karma:</td><td>') + 30
 		karmaEnd = source.find('</td>', karmaStart)
 		self.karma = int(source[karmaStart:karmaEnd])
+
+hn = HackerNewsAPI()
+stories = hn.getNewestStories()
+
+for s in stories:
+	s.printDetails()
+
+
